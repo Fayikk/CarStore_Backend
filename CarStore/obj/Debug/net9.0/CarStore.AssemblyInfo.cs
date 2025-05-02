a5ab1679c6b403b6c669d3c9b28a2056c23c8e68
Fayikk <veznedaroglufayik2@gmail.com>
2025-05-02 17:13:17 +0300

    changeYmlFile

--- CarStore/obj/Debug/net9.0/CarStore.AssemblyInfo.cs
+++ CarStore/obj/Debug/net9.0/CarStore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cfa953fd92fc49945c3fa9f565e2e53642517a93")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cbe6da4077a725847ac0612cdddf27575c8da086")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
